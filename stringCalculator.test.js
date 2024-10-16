7537618cf488848de925f7a9f7f8963874e37dbf
abhishek <i.abhishek8899@gmail.com>
2024-10-16 22:09:32 +0530

    feat: handle multiple numbers input in Add method

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,17 +1,24 @@
 const { Add } = require("./stringCalculator");
 
-
 describe('String Calculator - Add Method', () => {
 
+    // Test case for handling an empty string input
     it('should return 0 for an empty string', () => {
         expect(Add("")).toBe(0);
     });
     
+    // Test case for validating the addition of a single number
     it('should return the number for a single number input', () => {
         expect(Add("1")).toBe(1);
     });
     
+    // Test case for validating the addition of two comma-separated numbers
     it('should return the sum of two numbers when passed', () => {
         expect(Add("3,7")).toBe(10);
+    });
+    
+    // Test case for validating the addition of multiple comma-separated numbers
+    it('should return the sum of multiple numbers', () => {
+        expect(Add("1,2,3,4")).toBe(10);
     });
 });
